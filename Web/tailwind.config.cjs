d2847a52e1edb9a98e8772c15b1991568ac9daa7
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2023-01-18 22:02:07 -0300

    feat: add pallete theme colors

--- Web/tailwind.config.cjs
+++ Web/tailwind.config.cjs
@@ -4,7 +4,23 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
 ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        WHITE: '#FDFFFF',
+        DARK_WHITE: '#FDFFFF',
+        GRAY: '#D9D9D9',
+        DARK_GRAY: '#646464',
+        BLACK: '#222222',
+        
+        PRINCIPAL_GREEN : '#00C44B',
+        
+        RED: '#F35757',
+        LIGHT_BLUE: '#0FA0E3',
+        DARK_BLUE: '#5045E6',
+        LIGHT_YELLOW: '#FFCC4D',
+        DARK_YELLOW: '#FFAC33'
+      }
+    },
   },
   plugins: [],
 }
