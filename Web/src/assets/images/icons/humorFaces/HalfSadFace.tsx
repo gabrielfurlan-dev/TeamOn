af66d94ffa40fc7ca1fe95d8e45bf9ce32307c38
Gabriel Furlan <gabrielcarlos1023@gmail.com>
2023-01-22 14:39:16 -0300

    fix: correct svg files

--- Web/src/assets/images/icons/humorFaces/HalfSadFace.tsx
+++ Web/src/assets/images/icons/humorFaces/HalfSadFace.tsx
@@ -11,9 +11,6 @@ const HalfSadFace: React.FC<FacesProps> = ({ width, height, humor }) => {
             fill={humor == EHumorStatus.HalfSad ? "#FFCC4D" : "none"}
             xmlns="http://www.w3.org/2000/svg">
 
-            <circle cx="12.5898" cy="18.5" r="2.5" fill="#646464" />
-            <circle cx="23.5898" cy="18.5" r="2.5" fill="#646464" />
-
             <path
                 d="M14.8898 26.5H21.2898M34.0898 18C34.0898 26.8368 26.9266 34 18.0898 34C9.25304 34 2.08984 26.8368 2.08984 18C2.08984 9.1632 9.25304 2 18.0898 2C26.9266 2 34.0898 9.1632 34.0898 18Z"
                 stroke="#646464"
@@ -21,6 +18,9 @@ const HalfSadFace: React.FC<FacesProps> = ({ width, height, humor }) => {
                 stroke-linecap="round"
                 stroke-linejoin="round"
             />
+
+            <circle cx="12.5898" cy="18.5" r="2.5" fill="#646464" />
+            <circle cx="23.5898" cy="18.5" r="2.5" fill="#646464" />
 
         </svg>
     );
