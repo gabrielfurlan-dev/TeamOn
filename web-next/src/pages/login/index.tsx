4199a7d7cbccf2cc78984ee8418eb72ac8fb15a7
Gabriel Garcia <gokugamer190@gmail.com>
2023-02-15 20:01:56 -0300

    feat: add logo teamon component

---
+++ web-next/src/pages/login/index.tsx
@@ -0,0 +1,10 @@
+import Logo from "@/assets/Logo_TeamOn";
+
+export default function Login() {
+    return (
+        <div>
+            <Logo />
+            <h1>TELA DE LOGIN</h1>
+        </div>
+    );
+}
