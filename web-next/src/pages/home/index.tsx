35c1d3836e3322162f3c8e4f1e9a2279875cf6a5
Tamicktom <rickpokemon38@gmail.com>
2023-04-10 19:54:55 -0300

    fix tailwind
    Sério, não precisa importar React.

--- web-next/src/pages/home/index.tsx
+++ web-next/src/pages/home/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Humour from '../humour';
 import { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import { IHumour, SendHumourProps } from '@/Interfaces/Humour';
@@ -18,34 +17,21 @@ export const getServerSideProps: GetServerSideProps<SendHumourProps> = async (co
 
 export default function Home({ humours }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return (
-
         <NavBar>
-            <div className='bg-LIGHT_GRAY h-screen w-screen'>
-
-                <div className='bg-WHITE mt-4 mr-4 ml-4 lg:h-1/4 rounded-xl'>
+            <div className='w-screen h-screen bg-LIGHT_GRAY'>
+                <div className='mt-4 ml-4 mr-4 bg-WHITE lg:h-1/4 rounded-xl'>
                     <TopBar />
                 </div>
-
                 <div className='flex  lg:flex-row flex-col lg:h-[73%]' >
-
-                    <div className='bg-WHITE m-4 lg:w-8/12 rounded-xl' >
+                    <div className='m-4 bg-WHITE lg:w-8/12 rounded-xl' >
                         <Humour humours={humours} />
                     </div>
-
-                    <div className='bg-WHITE
-                                  lg:mt-4 lg:ml-0
-                                  mx-4 mb-4
-                                  lg:w-4/12
-                                  rounded-xl
-                                  flex flex-col items-center justify-center' >
-
+                    {/* never write className like this, dont break the line */}
+                    <div className='flex flex-col items-center justify-center mx-4 mb-4 bg-WHITE lg:mt-4 lg:ml-0 lg:w-4/12 rounded-xl' >
                         <p className='text-RED'>Ranking</p>
-
                     </div>
                 </div>
-
             </div>
-
         </NavBar>
     );
 }
